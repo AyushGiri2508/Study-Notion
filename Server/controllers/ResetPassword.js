d7b5a6e694188dcf4d26db25db2a2e9b55b23281
AyushGiri2508 <ayushgiri9988777@gmail.com>
2026-02-23 00:16:37 +0530

    Updating Controller

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -1,6 +1,48 @@
+  const User = require("../models/User");
+  const mailSender = require("../utils/mailSender");
 
 
 // resetPasswordToken
+exports.resetPasswordToken= async (req,res)=>{
+    try {
+
+        //    get email from req.body
+const email=req.body.email;
+//   check if user exists for this email, if not send response
+const user= await User.findOne({email:email});
+if(!user){
+    return res.status(400).json({
+        success:false,
+        message:"Your email is not registerd with us"
+    });
+}
+// generate token
+const token=crypto.randomUUID();
+// update user by adding token and expiration time
+const updatedDetails= await User.findOneAndUpdate({email:email},{
+    token:token,
+    resetPasswordExpires:Date.now()+5*60*1000,
+},{new:true});
+// create url
+const url= `http://localhost:3000/update-password/${token}`;
+// send email to user with the url
+await mailsender(email,"Password reset link",`Click on the link to reset your password ${url} . This link is valid for 5 minutes`);
+
+// send response
+return res.status(200).json({
+    success:true,
+    message:"Password reset link has been sent to your email account, please check your inbox",
+})
+ 
+
+    } catch(error) {
+
+        
+    }
+
+
+
+}
 
 
 
--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -3,14 +3,73 @@ require('dotenv').config();
 const User=require('../models/User');
 
 //auth
+exports.auth=async(req,res,next)=>{
+try{
+    // exract token from header
+    const token=req.cookies.token||req.body.token||req.header('Authorisation').replace('Bearer ','');
+    if(!token){
+        return res.status(401).json({success:false,message:'Not authorised to access this route'});
+    }
+    // verify token
+    try{
+            const decoded=jwt.verify(token,process.env.JWT_SECRET);
+            console.log(decoded);
+            req.user=decoded;
+    }catch{
+        // verification failed
+        return res.status(401).json({success:false,message:'Not authorised to access this route'});
+    }
+    next();
+}
+catch(err){
+    return res.status(401).json({success:false,message:'Server error ,something went wrong'});
+
+}
+}
 
 
 
 // isStudent
+exports.isStudent=async(req,res,next)=>{
+    try{
+        if(req.user.accountType!=='Student'){
+            return res.status(401).json({success:false,message:'This is proted rote only for students, please login as a student to access'});
+        }
+
+    }catch{
+        return res.status(401).json({success:false,message:'user role cananot be verified,please login again'});
+
+    }
+
+}
 
 
 
 // isAdmin
+exports.isAdmin=async(req,res,next)=>{
+    try{
+        if(req.user.accountType!=='Admin'){
+            return res.status(401).json({success:false,message:'This is proted rote only for Admin, please login as a student to access'});
+        }
+
+    }catch{
+        return res.status(401).json({success:false,message:'user role cananot be verified,please login again'});
+
+    }
+
+}
 
 
 // isInstructor
+exports.isInstructor=async(req,res,next)=>{
+    try{
+        if(req.user.accountType!=='Instructor'){
+            return res.status(401).json({success:false,message:'This is proted rote only for Instructor only, please login as a student to access'});
+        }
+
+    }catch{
+        return res.status(401).json({success:false,message:'user role cananot be verified,please login again'});
+
+    }
+
+}
